724d02f22b19d94903e1fd3be2b2c1976a5e335a
caturhidayat <c.meong@gmail.com>
2021-11-16 18:54:58 +0700

    Create random function

--- script.js
+++ script.js
@@ -1,1 +1,7 @@
-console.log('test file`')
+function randomDiffMessages(num) {
+    const random= Math.floor(Math.random() * num)
+    console.log(`This is a ${random}`);
+}
+
+
+randomDiffMessages(8)
