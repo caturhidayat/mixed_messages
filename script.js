c447bb601b77d15a2347d7b61036d197bafacbb6
caturhidayat <c.meong@gmail.com>
2021-11-16 19:05:44 +0700

    Create quote object

--- script.js
+++ script.js
@@ -5,3 +5,39 @@ function randomDiffMessages(num) {
 
 
 randomDiffMessages(8)
+
+const quoteRandom = {
+    quote = {
+        name: 'Robin Sharma',
+        quote: 'We grow fearless when we do the things we fear.'
+    },
+    quote = {
+        name: 'Paulo Coelho',
+        quote: 'And, when you want something, all the universe conspires in helping you to achieve it'
+    },
+    quote = {
+        name: 'Vincent Van Gogh',
+        quote: "If you hear a voice within you say 'you cannot paint,' then by all means paint and that voice will be silenced"
+    },
+    quote = {
+        name: 'Robin Sharma',
+        quote: "You can't be great if you don't feel great. Make exceptional health your number one priority"
+    },
+    quote = {
+        name: 'Paulo Coelho',
+        quote: 'Impossible is just an opinion'
+    },
+    quote = {
+        name: 'Mark Twain',
+        quote: 'The secret of getting ahead is getting started.'
+    },
+    quote = {
+        name: 'Walt Disney',
+        quote: 'All our dreams can come true if we have the courage to pursue them.'
+    },
+    quote = {
+        name: 'Paulo Coelho',
+        quote: 'Waiting is painful. Forgetting is painful. But not knowing which to do is the worst kind of suffering.'
+    }
+
+}
