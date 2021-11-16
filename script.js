d095c5e4238f96b7377f2240ddfd89a16de4a2d0
caturhidayat <c.meong@gmail.com>
2021-11-16 19:17:37 +0700

    Create suffle function - empty

--- script.js
+++ script.js
@@ -6,38 +6,14 @@ function randomDiffMessages(num) {
 
 randomDiffMessages(8)
 
-const quoteRandom = {
-    quote = {
-        name: 'Robin Sharma',
-        quote: 'We grow fearless when we do the things we fear.'
-    },
-    quote = {
-        name: 'Paulo Coelho',
-        quote: 'And, when you want something, all the universe conspires in helping you to achieve it'
-    },
-    quote = {
-        name: 'Vincent Van Gogh',
-        quote: "If you hear a voice within you say 'you cannot paint,' then by all means paint and that voice will be silenced"
-    },
-    quote = {
-        name: 'Robin Sharma',
-        quote: "You can't be great if you don't feel great. Make exceptional health your number one priority"
-    },
-    quote = {
-        name: 'Paulo Coelho',
-        quote: 'Impossible is just an opinion'
-    },
-    quote = {
-        name: 'Mark Twain',
-        quote: 'The secret of getting ahead is getting started.'
-    },
-    quote = {
-        name: 'Walt Disney',
-        quote: 'All our dreams can come true if we have the courage to pursue them.'
-    },
-    quote = {
-        name: 'Paulo Coelho',
-        quote: 'Waiting is painful. Forgetting is painful. But not knowing which to do is the worst kind of suffering.'
-    }
+const quoteRandom = [
+    'We grow fearless when we do the things we fear. -Robin Sharma', 
+    'And, when you want something, all the universe conspires in helping you to achieve it. -Paulo Coelho', 
+    "If you hear a voice within you say 'you cannot paint,' then by all means paint and that voice will be silenced. -Vincent Van Gogh", 
+    "You can't be great if you don't feel great. Make exceptional health your number one priority. -Robin Sharma", 'Impossible is just an opinion. -Paulo Coelho', 
+    'The secret of getting ahead is getting started. -Mark Twain']
+
+function suffleNumber(num) {
+    console.log(`The number is : ${num}`)
 
 }
