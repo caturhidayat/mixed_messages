6a68d1f2d446b71ac6e60d10fdf6492c721b1d65
caturhidayat <c.meong@gmail.com>
2021-11-18 13:41:40 +0700

    create collectiveWisdom object

--- script.js
+++ script.js
@@ -1,3 +1,12 @@
 function randomDiffMessages(num) {
     const random= Math.floor(Math.random() * num)
 }
+
+
+const collectiveWisdom = {
+    signInfo : ['Moon', 'Stars', 'Comet', 'Sky'],
+    fortuneOutput : ['terrible luck', 'bad luck', 'ok luck', 'good luck', 'great luck'],
+    advice: ['go out and eat', 'not read this', 'play more', 'trust no one']
+}
+
+
