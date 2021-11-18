26e38a487e0b217f437b8d4a2938ffd24c79e621
caturhidayat <c.meong@gmail.com>
2021-11-18 13:55:12 +0700

    loop & switch case collectiveWisdom and formatter function

--- script.js
+++ script.js
@@ -9,4 +9,29 @@ const collectiveWisdom = {
     advice: ['go out and eat', 'not read this', 'play more', 'trust no one']
 }
 
+const personalWisdom = []
 
+for(let prop in collectiveWisdom) {
+    const index = randomDiffMessages(collectiveWisdom[prop].length)
+
+    switch (prop) {
+        case 'signInfo':
+            personalWisdom.push(`Your sign is ${collectiveWisdom[prop][index]}`)
+            break;
+        case 'fortuneOutput':
+            personalWisdom.push(`Your are having ${collectiveWisdom[prop][index]}`)
+            break;
+        case 'advice':
+            personalWisdom.push(`Your should ${collectiveWisdom[prop][index]}`)
+            break;
+        default:
+            personalWisdom.push(`There is not enough info`)
+            break;
+    }
+}
+
+function formatWisdom(wisdom) {
+    const formatter = personalWisdom.join("\n")
+}
+
+formatWisdom(personalWisdom);
