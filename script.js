e0dc03ff1653caf323eaa01c03d1a78c4c3d116b
caturhidayat <c.meong@gmail.com>
2021-11-18 13:40:33 +0700

    clear code, create new random diff

--- script.js
+++ script.js
@@ -1,19 +1,3 @@
 function randomDiffMessages(num) {
     const random= Math.floor(Math.random() * num)
-    console.log(`This is a ${random}`);
-}
-
-
-randomDiffMessages(8)
-
-const quoteRandom = [
-    'We grow fearless when we do the things we fear. -Robin Sharma', 
-    'And, when you want something, all the universe conspires in helping you to achieve it. -Paulo Coelho', 
-    "If you hear a voice within you say 'you cannot paint,' then by all means paint and that voice will be silenced. -Vincent Van Gogh", 
-    "You can't be great if you don't feel great. Make exceptional health your number one priority. -Robin Sharma", 'Impossible is just an opinion. -Paulo Coelho', 
-    'The secret of getting ahead is getting started. -Mark Twain']
-
-function suffleNumber(num) {
-    console.log(`The number is : ${num}`)
-
 }
